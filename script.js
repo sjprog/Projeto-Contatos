e0f6428fb8a20736bd232d2c86e03e6ce991a490
sidney junio de freitas alves siqueira <92001069+sjprog@users.noreply.github.com>
2022-03-12 16:58:58 -0300

    valores

--- script.js
+++ script.js
@@ -1,12 +1,33 @@
-const nav = document.querySelector('.nav1')
-window.addEventListener('scroll', fixNav)
+// const nav = document.querySelector('.nav1')
+// window.addEventListener('scroll', fixNav)
 
-function fixNav() {
-    if(window.scrollY > nav.offsetHeight + 150) {
-        nav.classList.add('active')
-    } else {
-        nav.classList.remove('active')
-    }
+// function fixNav() {
+//     if(window.scrollY > nav.offsetHeight + 150) {
+//         nav.classList.add('active')
+//     } else {
+//         nav.classList.remove('active')
+//     }
+// }
+
+
+const boxes = document.querySelectorAll('.box')
+
+window.addEventListener('scroll', checkBoxes)
+
+checkBoxes()
+
+function checkBoxes() {
+    const triggerBottom = window.innerHeight / 5 * 4
+
+    boxes.forEach(box => {
+        const boxTop = box.getBoundingClientRect().top
+
+        if(boxTop < triggerBottom) {
+            box.classList.add('show')
+        } else {
+            box.classList.remove('show')
+        }
+    })
 }
 
 
